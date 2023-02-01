d0ef3de61a93cccb8ad33cfe8b8c62c0253ed19d
ch <mkmsacc@outlook.com>
2023-02-01 14:42:58 +0700

    Refactor test and new req

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hippo")
-	want := "Hello, Hippo"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Hippo")
+		want := "Hello, Hippo"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, Hippo' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Hippo"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got string, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
