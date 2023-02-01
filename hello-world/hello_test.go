1730afb312d62e34e5736fb641f0ee682eaf64de
ch <mkmsacc@outlook.com>
2023-02-01 16:31:31 +0700

    Implement New req test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Hippo")
+		got := Hello("Hippo", "")
 		want := "Hello, Hippo"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, Hippo' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, Hippo"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Hippo", "Spanish")
+		want := "Hola, Hippo"
 		assertCorrectMessage(t, got, want)
 	})
 }
