7cabff508bb2c1f08f519aa7ff4e058193122eb8
ch <mkmsacc@outlook.com>
2023-02-06 10:34:40 +0700

    Refactor tests

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -22,9 +22,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	checkSums(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T) {
@@ -33,18 +31,20 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
+}
+
+func checkSums(t *testing.T, got []int, want []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
