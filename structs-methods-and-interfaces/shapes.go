bcc0bcc6fb0d5b1e0bb6ccbfaac612ea827e481b
ch <mkmsacc@outlook.com>
2023-02-07 10:35:51 +0700

    Write code for Area

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -3,3 +3,6 @@ package structs_methods_and_interfaces
 func Perimeter(width float64, height float64) float64 {
 	return width + height + width + height
 }
+func Area(width float64, height float64) float64 {
+	return width * height
+}
