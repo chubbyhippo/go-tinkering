16a9599d030cc096d9670d013064ff295286fb04
ch <mkmsacc@outlook.com>
2023-02-08 10:53:40 +0700

    Refactor test

--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -12,25 +12,26 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	t.Run("rectanngles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
+func checkArea(t *testing.T, shape Shape, want float64) {
+	got := shape.Area()
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 
 }
