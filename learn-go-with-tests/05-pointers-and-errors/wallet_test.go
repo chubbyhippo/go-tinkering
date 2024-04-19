ab555601038b6085dc9dbe8b3b39b6bba0470345
chubbyhippo <mkmsacc@outlook.com>
2024-04-19 22:46:49 +0700

    Refactor tests for wallet functions
    The testing functions for deposit and withdraw have been simplified and refactored for clarity. Assertion functions have been introduced to reduce redundancy in test code. Also, the package name has been simplified from "_5_pointers_and_errors" to "pointers_and_errors".

--- learn-go-with-tests/05-pointers-and-errors/wallet_test.go
+++ learn-go-with-tests/05-pointers-and-errors/wallet_test.go
@@ -1,35 +1,60 @@
-package _5_pointers_and_errors
+package pointers_and_errors
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	t.Run("should deposit", func(t *testing.T) {
+	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("should withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: 20}
-		wallet.Withdraw(Bitcoin(10))
+	t.Run("withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if !errors.Is(got, want) {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
