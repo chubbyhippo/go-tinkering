c0dc9e4117514b2b01ba632148eb1a0743f50bbf
chubbyhippo <mkmsacc@outlook.com>
2024-04-19 20:57:55 +0700

    Add new test case for set operations
    A new test case has been added to set_test.go to check the functionality of the set operations. Specifically, it tests whether an item can be added to the set, and then deleted, as expected.

--- set/set_test.go
+++ set/set_test.go
@@ -12,5 +12,15 @@ func TestSet(t *testing.T) {
 			t.Errorf("got %t is not true", got)
 		}
 	})
+	t.Run("should add item to the set", func(t *testing.T) {
+		s := NewSet()
+		s.Add("Go")
+		s.Delete("Go")
+		got := s.Contains("Go")
+
+		if got != false {
+			t.Errorf("got %t is not false", got)
+		}
+	})
 
 }
