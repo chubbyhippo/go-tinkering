6394ee7f18c99c1750ab0815839917354883338c
chubbyhippo <mkmsacc@outlook.com>
2024-04-21 11:41:18 +0700

    Update dictionary test in learn-go-with-tests
    The dictionary test in the 6th lesson of learn-go-with-tests was modified to include error handling. An additional test case was introduced to handle the scenario when an unknown word

--- learn-go-with-tests/06-maps/dictionary_test.go
+++ learn-go-with-tests/06-maps/dictionary_test.go
@@ -6,11 +6,22 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("should return known word", func(t *testing.T) {
-		got := dictionary.Search("test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
 	})
+
+	t.Run("should return unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected error, got none")
+		}
+		assertStrings(t, err.Error(), want)
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
