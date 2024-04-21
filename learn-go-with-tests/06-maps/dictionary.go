f51b3594cda10f071af4d54684ac7e87619e783e
chubbyhippo <mkmsacc@outlook.com>
2024-04-21 11:20:42 +0700

    Refactor search function for dictionary
    The search function has been refactored to become a method of a new Dictionary type. Tests have also been updated to reflect this change. Additionally, an `assertStrings` function has been added to simplify the test assertions.

--- learn-go-with-tests/06-maps/dictionary.go
+++ learn-go-with-tests/06-maps/dictionary.go
@@ -1,5 +1,7 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return "this is just a test"
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) string {
+	return d[key]
 }
