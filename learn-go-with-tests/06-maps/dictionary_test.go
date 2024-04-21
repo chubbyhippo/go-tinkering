301fd27ee72c3d2171270a785d30533606657c92
chubbyhippo <mkmsacc@outlook.com>
2024-04-21 11:23:11 +0700

    Refactor dictionary test for readability
    Wrapped the dictionary search test within a subtest for better code readability and structure. The change helps to specify the exact functionality being tested, in this case, returning a known word. Removed an unnecessary line break at the end of the test.

--- learn-go-with-tests/06-maps/dictionary_test.go
+++ learn-go-with-tests/06-maps/dictionary_test.go
@@ -5,10 +5,12 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("should return known word", func(t *testing.T) {
+		got := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -17,5 +19,4 @@ func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
-
 }
