7078f77a3c1ad80c37453e00c443c40337276d73
ch <mkmsacc@outlook.com>
2024-04-23 10:22:03 +0700

    Add test case for "Add" method in dictionary
    A new test function "TestAdd" is added to the dictionary_test file. This new test is to verify the functionality of the "Add" method in the Dictionary type of our go program. The test ensures that the word added to the dictionary can be correctly retrieved later.

--- learn-go-with-tests/06-maps/dictionary_test.go
+++ learn-go-with-tests/06-maps/dictionary_test.go
@@ -24,6 +24,20 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
+
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
@@ -32,4 +46,4 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 
-// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#write-the-test-first-2
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#pointers-copies-et-al
