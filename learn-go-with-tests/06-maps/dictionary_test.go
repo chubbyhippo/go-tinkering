8d7befea674be08bb572416e445148cbd34f4e71
ch <mkmsacc@outlook.com>
2024-04-26 12:42:44 +0700

    Refactor TestUpdate function in dictionary_test.go
    The TestUpdate function has been refactored to include t.Run() for better test isolation and clarity. The function logic remains the same - it updates the definition for a given word in the dictionary, and then verifies the update is successful.

--- learn-go-with-tests/06-maps/dictionary_test.go
+++ learn-go-with-tests/06-maps/dictionary_test.go
@@ -52,14 +52,16 @@ func TestAdd(t *testing.T) {
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#write-the-test-first-5
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("should update word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
 
 }
 
