39db9a8982a5c609c34e298ce80b0270d49d2a08
ch <mkmsacc@outlook.com>
2024-05-08 13:32:32 +0700

    Refactor Countdown function to use io.Writer
    The main function, Countdown, has been refactored to take an io.Writer as a parameter. This allows for better testing capabilities and more flexible output destinations. The associated test function has been altered to utilize this change and to verify the output against a string buffer.

--- learn-go-with-tests/08-mocking/countdown.go
+++ learn-go-with-tests/08-mocking/countdown.go
@@ -1,10 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	Countdown()
+	Countdown(os.Stdout)
 }
 
-// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
-func Countdown() {
-
+func Countdown(out io.Writer) {
+	fmt.Fprintf(out, "3")
 }
+
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking#write-the-test-first-1
